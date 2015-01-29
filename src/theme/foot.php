5f42c67df7cdde79e95df2fecd00ef1d686472e1
Josemi <jmjuanes@outlook.es>
2015-01-29 11:02:54 +0100

    Added counter

--- src/theme/foot.php
+++ src/theme/foot.php
@@ -31,3 +31,22 @@
 	<!-- Espacios -->
 	<br><br><br>
 </div>
+
+<!-- Start of StatCounter Code for Default Guide -->
+<script type="text/javascript">
+var sc_project=10261714; 
+var sc_invisible=1; 
+var sc_security="9e672b60"; 
+var scJsHost = (("https:" == document.location.protocol) ?
+"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" +
+scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
+</script>
+<noscript>
+<div class="statcounter"><a title="free web stats"
+href="http://statcounter.com/" target="_blank"><img
+class="statcounter"
+src="http://c.statcounter.com/10261714/0/9e672b60/1/"
+alt="free web stats"></a></div></noscript>
+<!-- End of StatCounter Code for Default Guide -->
