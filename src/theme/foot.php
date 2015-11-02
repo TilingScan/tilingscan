002ea2d98af0c13ce193c7f58b01c1872c6bf4e9
jmjuanes <josemijuanes@gmail.com>
2015-11-02 18:00:37 +0100

    More changes

--- src/theme/foot.php
+++ src/theme/foot.php
@@ -1,15 +1,15 @@
 <!-- Pie de pagina -->
 <div align="center" class="foot">
-	
+
 	<!-- Espacios -->
 	<br>
-	
+
 	<!-- Logo -->
 	<a href="./" class="foot-logo">TilingScan</a>
-	
+
 	<!-- Espacios -->
 	<br>
-	
+
 	<!-- Enlaces -->
 	<a href="./" class="foot-block">Home</a>
 	<a href="./tutorial" class="foot-block">Tutorial</a>
@@ -17,26 +17,26 @@
 	<a href="./dashboard" class="foot-block">Dashboard</a>
 	<a href="http://jmjuanes.github.io/annotate" class="foot-block" target="_blank">Annotate!</a>
 	<a href="https://github.com/TilingScan" class="foot-block" target="_blank">GitHub</a>
-	
+
 	<!-- Espacios -->
 	<br><br>
-	
+
 	<!-- Autores -->
-	&copy; 2014 Vicente Arnau, Jose Enrique Perez, Ana Miguel & 
-	<a href="http://www.jmjuanes.es" class="foot-text" target="_blank" >Jose M. Juanes</a>
-	
+	&copy; 2014 Vicente Arnau, Jose Enrique Perez, Ana Miguel &amp;
+	<a href="http://jm.juanes.xyz" class="foot-text" target="_blank" >Jose M. Juanes</a>
+
 	<!-- Universidad -->
 	<br><br><a href="http://www.uv.es/" target="_blank" class="foot-uv">Universidad de Valencia - Spain</a>
-	
+
 	<!-- Espacios -->
 	<br><br><br>
 </div>
 
 <!-- Start of StatCounter Code for Default Guide -->
 <script type="text/javascript">
-var sc_project=10261714; 
-var sc_invisible=1; 
-var sc_security="9e672b60"; 
+var sc_project=10261714;
+var sc_invisible=1;
+var sc_security="9e672b60";
 var scJsHost = (("https:" == document.location.protocol) ?
 "https://secure." : "http://www.");
 document.write("<sc"+"ript type='text/javascript' src='" +
--- support.php
+++ support.php
@@ -3,52 +3,49 @@
 	<head>
 		<!-- Head -->
 		<?php require 'src/theme/head.php'; ?>
-		
+
 		<!-- Titulo de la web -->
 		<title>Support - TilingScan</title>
 	</head>
 	<body>
 		<!-- Head -->
 		<?php require 'src/theme/menu.php'; ?>
-		
+
 		<!-- Principal -->
 		<div class="main" align="center">
-		
+
 			<!-- Espacios -->
 			<br><br>
-			
+
 			<!-- Titulo -->
 			<span class="h1">Support</span><br><br>
-			
+
 			<!-- Descripcion -->
 			<span class="h2">Do you need help? Want to report an issue? Contact us: </span>
-			
+
 			<!-- Espacios -->
 			<br><br><br>
-			
+
 			<!-- Tabla -->
-			<div class="table" align="center">
-				
-				<!-- Celda Tutorial -->
+			<div align="center">
+
+				<!-- Celda Soporte
 				<div class="table-cell">
 					<a href="mailto:amiblan@alumni.uv.es" class="table-btn background-blue">amiblan@alumni.uv.es</a>
 				</div>
-				
-				<!-- Celda Soporte -->
-				<div class="table-cell">
-					<a href="mailto:jmjuanes@outlook.com" class="table-btn background-green">jmjuanes@outlook.com</a>
-				</div>
-				
+				 -->
+				<div class="table-btn background-green">J.Miguel.Juanes@uv.es</div>
+
 			</div>
-			
+
 		</div>
-		
+
 		<!-- Espacios -->
 		<br><br>
-		
-		
+
+
 		<!-- Pie de pagina -->
 		<?php require 'src/theme/foot.php'; ?>
-		
+
 	</body>
 </html>
